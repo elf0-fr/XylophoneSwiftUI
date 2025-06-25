db345c8592875a8ea9f102560302abaefcfd6c9c
Romain Tirbisch <romain_tirbisch@yahoo.com>
2025-06-25 19:17:01 +0200

    Initial Commit

---
+++ XylophoneSwiftUI/XylophoneSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  XylophoneSwiftUIApp.swift
+//  XylophoneSwiftUI
+//
+//  Created by Elfo on 25/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct XylophoneSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
